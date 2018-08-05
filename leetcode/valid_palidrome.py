3e781dd7da55d99aa7022921d7c6c17b54a757b4
conordewey3 <cdewey96@vt.edu>
2018-08-05 16:55:17 -0700

    Valid Palidrome

---
+++ leetcode/valid_palidrome.py
@@ -0,0 +1,23 @@
+
+'''
+125. Valid Palindrome
+
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+
+'''
+
+# Simplest, Pythonic Approach - O(n) time, O(1) space
+def isPalindrome(self, s):
+    s = [i.lower() for i in s if i.isalnum()]
+    return s == s[::-1]
