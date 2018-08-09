4d51409848846af79563b0cb0c099d48270e53ef
conordewey3 <cdewey96@vt.edu>
2018-08-08 20:12:42 -0700

    First unique char

---
+++ leetcode/first_unique_char.py
@@ -0,0 +1,34 @@
+
+'''
+
+387. First Unique Character in a String
+
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
+
+'''
+
+# Dictionary Approach - O(n) time, O(n) space
+def firstUniqChar(self, s):
+	seen = {}
+	for item in s:
+		if item not in seen:
+			seen[item] = 1
+		else:
+			seen[item] += 1
+
+	for i in range(len(s)):
+		item = s[i]
+		if seen[item] == 1:
+			return i
+	return -1
+
+
