ecae7ffbdefd5ff4c7daf6c2211fef0dcd3cb962
conordewey3 <cdewey96@vt.edu>
2018-10-17 15:46:33 -0400

    9 days

---
+++ sql/sql_exercises2.sql
@@ -0,0 +1,48 @@
+
+-- Link to questions: https://en.wikibooks.org/wiki/SQL_Exercises/Employee_management
+
+-- 2.1 Select the last name of all employees.
+
+-- 2.2 Select the last name of all employees, without duplicates.
+
+-- 2.3 Select all the data of employees whose last name is "Smith".
+
+-- 2.4 Select all the data of employees whose last name is "Smith" or "Doe".
+
+-- 2.5 Select all the data of employees that work in department 14.
+
+-- 2.6 Select all the data of employees that work in department 37 or department 77.
+
+-- 2.7 Select all the data of employees whose last name begins with an "S".
+
+-- 2.8 Select the sum of all the departments' budgets.
+
+-- 2.9 Select the number of employees in each department (you only need to show the department 
+-- code and the number of employees).
+
+-- 2.10 Select all the data of employees, including each employee's department's data.
+
+-- 2.11 Select the name and last name of each employee, along with the name and budget of the employee's department.
+
+-- 2.12 Select the name and last name of employees working for departments with a budget greater than $60,000.
+
+-- 2.13 Select the departments with a budget larger than the average budget of all the departments.
+
+-- 2.14 Select the names of departments with more than two employees.
+
+-- 2.15 Very Important - Select the name and last name of employees working for departments with second lowest budget.
+
+-- 2.16  Add a new department called "Quality Assurance", with a budget of $40,000 and departmental code 11. 
+
+-- And Add an employee called "Mary Moore" in that department, with SSN 847-21-9811.
+
+-- 2.17 Reduce the budget of all departments by 10%.
+
+-- 2.18 Reassign all employees from the Research department (code 77) to the IT department (code 14).
+
+-- 2.19 Delete from the table all employees in the IT department (code 14).
+
+-- 2.20 Delete from the table all employees who work in departments with a budget greater than or equal to $60,000.
+
+-- 2.21 Delete from the table all employees.
+
